26ca25e09ecba916008df1ffb04d73c3faf18dba
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2024-11-29 13:32:16 +0100

    [EEE-325][ADD] create the prototype product card

--- src/pages/shop/seller.tsx
+++ src/pages/shop/seller.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
-import Image from 'next/image';
 import Layout from '@/components/Layout';
 import Loading from '@/components/Loading';
+import { useRouter } from 'next/router';
 
 const Shop = () => {
   const { data: session } = useSession();
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  console.log(session);
+
+  
 
   useEffect(() => {
     console.log("useEffect Fetching Seller's Shop");
@@ -34,23 +39,23 @@ const Shop = () => {
   
   return (
     <Layout>
-      <div className="bg-sky-950 bg-opacity-90 min-h-screen flex flex-row">
+      <div className="bg-[url('/images/shop/shop_background.png')] bg-cover bg-center bg-opacity-90 min-h-screen flex flex-row">
         {/* aqui va ha estar el vendedor y la informacion sobre que tienda es */}
-        <div className="w-3/12 p-4 bg-fuchsia-700 bg-opacity-10 flex flex-col items-center">
+        <div className="w-3/12 p-4 bg-black bg-opacity-70 flex flex-col items-center">
           <div className="flex space-x-4">
             {/* este es el cartel que va atener el boton para hechar para atras */}
             <div className="relative w-1/4">
               <img
                 src="/images/shop/cartel_colgante.png"
-                alt="Imagen 1"
-                className="w-full h-full"
+                alt="hanging sign small"
+                className="w-full h-3/4"
               />
               {/* aqui va ha estar el texto de que tipo de vendedor es */}
-                <div className="absolute inset-0 top-1/4 flex items-center justify-center">
-                    <button onClick={() => console.log('Botón clicado')}>
+                <div className="relative inset-0 -top-24 flex items-center justify-center">
+                    <button onClick={() => router.push('/shop')}>
                         <img
                             src="/images/shop/flecha_retroceso.png"
-                            alt="Botón"
+                            alt="back arrow"
                             className="w-16 h-16 cursor-pointer"
                         />
                     </button>
@@ -60,22 +65,67 @@ const Shop = () => {
             <div className="relative w-3/4">
               <img
                 src="/images/shop/cartel_colgante.png"
-                alt="Imagen 2"
+                alt="hanging sign"
                 className="w-full"
               />
               {/* aqui va ha estar el texto de que tipo de vendedor es */}
-              <div className="absolute inset-0 top-1/4 flex items-center justify-center">
-
+              <div className="relative inset-0 -top-24 flex items-center justify-center">
                 <p className="text-white text-6xl font-bold">The Seller</p>
               </div>
             </div>
           </div>
+
+          <div>
+            <img
+                  src="/images/shop/seller.png"
+                  alt="hanging sign"
+                  className="w-full"
+                />
+          </div>
+          <div className="w-full h-full p-4 bg-red-900 bg-opacity-10 flex items-center justify-center">
+
+          </div>
         </div>
+
         {/* aqui va ha estar el objeto que tengas seleccionado del inventario */}
-        <div className="w-5/12 p-4 bg-red-900 bg-opacity-10 flex items-center justify-center">
+        <div className="w-5/12 bg-red-900 bg-opacity-10 flex flex-col items-center rounded-3xl shadow-lg">
+          {/* este es el contenedor que crea la tarjeta */}
+          <div className="w-3/5 h-4/6 p-4 bg-black bg-opacity-70 flex flex-col items-center rounded-2xl shadow-lg border-4 border-sepia">
+            
+            {/* nombre de el objeto */}
+            <div className="w-full text-center bg-medievalSepia py-2 rounded-t-2xl">
+              <p className="text-black text-4xl font-bold">The Seller</p>
+            </div>
+            
+            {/* aquie va la imagen del ojeto que sea */}
+            <div className="w-3/5 mt-4 border-4 border-sepia rounded-xl overflow-hidden shadow-md bg-white">
+              <img
+                src="/images/equipment/armors/full_plate_28.png"
+                alt="armor"
+                className="w-full h-auto"
+              />
+            </div>
+            
+            {/* aqui aparecera la descripcin del objeto que tengas seleccionado */}
+            <div className="w-full mt-4 text-center">
+              <p className="text-white text-3xl font-medium">description</p>
+            </div>
+            
+            {/* Precio base del objeto */}
+            <div className="w-auto -bottom-12 mt-auto text-center bg-medievalSepia py-1 px-2 rounded-2xl flex flex-row items-center justify-center relative">
+              <p className="text-black text-4xl font-bold mr-2">937</p>
+              <img
+                src="/images/shop/gold.png"
+                alt="gold coin"
+                className="w-12 h-12 ml-2"
+              />
+            </div>
+          </div>
         </div>
+
         {/* aqui va ha estar el inventario en el que vas a poder seleccionar los objetos para luego venderlos */}
-        <div className="w-4/12 p-4 bg-lime-500 bg-opacity-10 flex items-center justify-center">
+        <div className="w-4/12 p-4 bg-black bg-opacity-70 flex items-center justify-center">
+
         </div>
       </div>
     </Layout>
