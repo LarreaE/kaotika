48f826159a37b306057990874a97a6d5c0a1fc05
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-11-29 09:54:26 +0100

    [EEE-309][UPDATE] getrandomitems by collection

--- src/helpers/getRandomItems.ts
+++ src/helpers/getRandomItems.ts
@@ -1,4 +1,7 @@
-export const getRandomItems = (items: any[], count: number) => {
-    const shuffled = items.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, count);
-  };
+import mongoose from '@/DB/mongoose/config';
+
+export const getRandomItems = async (collectionName: string, count: number) => {
+  const items = await mongoose.connection.collection(collectionName).find({}).toArray();
+  const shuffled = items.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, count);
+};
