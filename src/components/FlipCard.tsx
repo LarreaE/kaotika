5f506eb7a922d729253337c9a5890e50f861959f
ionpazoos <jon.pazos@ikasle.aeg.eus>
2024-12-02 10:21:44 +0100

    [EEE-307][ADD]added border to description

--- src/components/FlipCard.tsx
+++ src/components/FlipCard.tsx
@@ -12,21 +12,37 @@ interface FlipCardProps {
 const FlipCard: React.FC<FlipCardProps> = ({ name, description, merchantId, profession }) => {
   return (
     <Link legacyBehavior href={`/shop/${profession}`}>
-      <a className="w-[250px] h-[500px] perspective-1000 block"> {/* Añadimos `block` para que el enlace abarque toda la tarjeta */}
+      <a className="w-[400px] h-[550px] perspective-1000 block"> {/* `block` asegura que el enlace abarque toda la tarjeta */}
         <div className="relative w-full h-full transition-transform duration-1000 transform-style-preserve-3d hover:rotate-y-180">
           {/* Frente de la tarjeta */}
-          <div className="absolute w-full h-full bg-gray-800 text-white flex justify-center items-center backface-hidden">
-            <img
-              src={`images/sellers/${merchantId}.png`}
-              alt={name}
-              className="w-full h-full object-cover"
-            />
+          <div className="absolute w-full h-full bg-gray-900 text-white flex justify-center items-center backface-hidden">
+            {/* Contenedor del borde */}
+            <div className="relative w-full h-full">
+                 {/* Imagen principal más pequeña */}
+              <img
+                src={`images/sellers/${merchantId}.png`}
+                alt={name}
+                className="relative w-[80%] h-[75%] object-cover z-20 mx-auto my-auto top-20"
+              />
+              {/* Imagen del borde */}
+              <img
+                src={`images/sellers/shop_border.png`}
+                alt="Border"
+                className="absolute top-0 left-0 w-[100%] h-[100%] object-contain z-30 pointer-events-none"
+              />
+            </div>
           </div>
           {/* Parte trasera de la tarjeta */}
-          <div className="absolute w-full h-full bg-gray-800 text-white flex flex-col justify-between items-center backface-hidden rotate-y-180 p-4">
-            <h1 className="text-center text-2xl font-bold mt-2 text-yellow-500">{name}</h1>
-            <p className="text-center m-4 text-lg leading-6">{description}</p>
-          </div>
+            <div className="absolute w-full h-full bg-gray-900 text-white flex flex-col items-center backface-hidden rotate-y-180 p-8 space-y-6">
+                <h1 className="text-center text-3xl font-bold text-yellow-500 mt-16">{name}</h1>
+                <p className="text-center text-xl leading-6 max-w-40">{description}</p>
+                {/* Imagen del borde */}
+                <img
+                    src={`images/sellers/shop_border_description.png`}
+                    alt="Border"
+                    className="absolute bottom-0 rigth-0 w-[100%] h-[100%] object-contain z-30 pointer-events-none"
+                />
+            </div>
         </div>
       </a>
     </Link>
--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -35,33 +35,36 @@ const Shop = () => {
   }
   return (
     <Layout>
-    <div className="relative flex justify-center items-center min-h-screen bg-gray-900 top-[70px] space-x-5">
+    <div className="relative flex justify-center items-center min-h-screen bg-gray-900 bottom-[50px] space-x-5">
     <FlipCard
         name="Alaric Gemwright"
-        description="Alaric Gemwright, a master jeweler of unmatched skill, crafts exquisite pieces sought after by royalty. Renowned for their intricate designs and flawless craftsmanship, his creations blend elegance, precision, and a touch of magic. Those who wear them often claim to feel imbued with grace and prestige, as if a part of Alaric’s spirit resides within his work."
+        description="Master jeweler of unmatched skill, crafts exquisite pieces sought after by royalty. Renowned for their intricate designs and flawless craftsmanship, his creations blend elegance, precision, and a touch of magic. Those who wear them often claim to feel imbued with grace and prestige."
         merchantId="seller1"
         profession='jeweler'
       />
       <FlipCard
         name="Eadric Steelwright"
-        description="Eadric Steelwright, master armorer of the north, forges armor of unmatched strength, beauty, and endurance, crafted to withstand even the fiercest battles. Each strike of his hammer echoes like a vow of victory, resonating through the halls of his legendary forge, which glows with the unrelenting fire of legends. His creations are not just tools of war, but symbols of honor and resilience, treasured by warriors and kings alike."
+        description="Eadric Steelwright, master armorer of the north, forges armor of unmatched strength and beauty, crafted to endure the fiercest battles. Each strike of his hammer echoes like a vow of victory, creating symbols of honor and resilience treasured by warriors and kings."
         merchantId="seller2"
         profession='armorsmith'
-
       />
       <FlipCard
         name="Quintus Wildbrew"
-        description="Quintus Wildbrew, the eccentric alchemist, is known for his chaotic experiments and an unmatched ability to concoct potions with unpredictable effects. His shop is a labyrinth of bubbling cauldrons, glowing vials, and rare ingredients, drawing curious adventurers and daring potion enthusiasts. With a mischievous grin and wild eyes, Quintus always has a “special brew” ready, though no one can predict exactly what it might do."
+        description="Quintus Wildbrew, the eccentric alchemist, is known for his chaotic experiments and unpredictable potions. His shop, filled with bubbling cauldrons and glowing vials, attracts adventurers seeking rare ingredients and daring creations."
         merchantId="seller3"
         profession='alchemist'
-
       />
       <FlipCard
         name="Draven Ironvalor"
-        description="Draven Ironvalor, the legendary master of shields and weapons, is a towering figure whose creations are revered by warriors and kings alike. In his blazing forge, he crafts unyielding shields that withstand the mightiest blows and blades so sharp they sing in battle. With arms like iron and a presence that commands respect, Draven’s every strike is a promise of unmatched strength. His masterpieces bear intricate carvings, telling stories of valor, and are said to carry the spirit of the forge itself, protecting those who wield them."
+        description="Draven Ironvalor, the legendary master of shields and weapons, is revered by warriors and kings. In his blazing forge, he crafts shields and blades so sharp they sing in battle. Each strike of his hammer is a promise of strength, and his masterpieces tell stories of valor and offer unmatched protection."
         merchantId="seller4"
         profession='weaponsmith'
-
+      />
+        <FlipCard
+        name="Magnus Tradebinder"
+        description="Magnus Tradebinder is a shrewd merchant with a knack for collecting treasures of all kinds, from rare artifacts to everyday goods. While his shop attracts adventurers and nobles alike, Magnus is known for driving a hard bargain, often squeezing every coin he can in a deal."
+        merchantId="buyer"
+        profession='buyer'
       />
     </div>
     </Layout>
