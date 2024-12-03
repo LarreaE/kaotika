e51243e37faa8147c4260e2a06d0287ba5f05512
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-12-03 10:46:54 +0100

    [EEE-316][ADD] instant buy working, update player in the screen required

--- src/pages/api/shop/[playerEmail]/[merchantId]/[itemId].ts
+++ src/pages/api/shop/[playerEmail]/[merchantId]/[itemId].ts
@@ -3,26 +3,62 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import mongoose from '@/DB/mongoose/config';
 import {Player} from '@/DB/mongoose/models/models'
+import { transformStringPlural,transformStringSingular } from '@/helpers/transformString';
 export default async (req: NextApiRequest, res: NextApiResponse)  => {
 
   const { playerEmail, merchantId ,itemId } = req.query;
-  console.log("Item ID: ", itemId);
 
   const transferItemToPlayer = async (playerEmail: any, merchantId: any, itemId: any) => {
     try {
 
-      const collectionName = `merchant_${merchantId}`;
+      const collectionName = transformStringSingular(merchantId);
       const model = mongoose.models[collectionName];
       if (!model) {
         throw new Error(`Model for collection "${collectionName}" not found.`);
       }
       const player = await Player.findOne({email: playerEmail})
 
-      const item = await model.findOne({ name: itemId });
+      console.log(collectionName);
+      console.log(mongoose.models);
       
-      console.log(item);
+      console.log("MODEL:", model);
+      console.log(itemId);
+
+      const fieldsToSearch = ['rings', 'artifacts', 'weapons','shields','helmets','armors','boots', 'ingredients'];
+
+      const items = await model.aggregate([
+        {
+          $project: {
+            allItems: {
+              $concatArrays: [
+                { $ifNull: ['$artifacts', []] },
+                { $ifNull: ['$rings', []] },
+                { $ifNull: ['$weapons', []] },
+                { $ifNull: ['$shields', []] },
+                { $ifNull: ['$helmets', []] },
+                { $ifNull: ['$armors', []] },
+                { $ifNull: ['$boots', []] },
+                { $ifNull: ['$ingredients', []] },
+              ]
+            }
+          }
+        },
+        {
+          $unwind: '$allItems'
+        },
+        {
+          $match: { 'allItems.name': itemId }
+        },
+        {
+          $replaceRoot: { newRoot: '$allItems' }
+        }
+      ]);
+
+      const item = items[0];
+
+      console.log("ITEM:", item);
       
-      if (!item?.value) {
+      if (!item) {
         throw new Error('Item not found or already sold');
       }
   
@@ -33,24 +69,23 @@ export default async (req: NextApiRequest, res: NextApiResponse)  => {
       const type = `${item.type}s`
       console.log(type);
       
-      console.log(player);
+      console.log(player.inventory);
+      console.log(player.inventory[type]);
       
-      const updatedInventory = player?.inventory[type].push(item);
+      await player?.inventory[type].push(item);
+      
 
-      console.log(updatedInventory);
+
+      console.log(player.inventory[type]);
 
       const updatedPlayer = await Player.findOneAndUpdate(
         { _id: player?._id },
         {
-          $set: { inventory: updatedInventory }, // adds the item
-          $inc: { gold: -item.value.value }, // take out the money
+          $set: { inventory: player.inventory }, // adds the inventory of the modified player
+          $inc: { gold: -item.value }, // take out the money
         },
         { returnDocument: 'after' } //return document after
       );
-  
-      if (!updatedPlayer?.value) {
-        throw new Error('Failed to update player inventory');
-      }
   
       console.log('Item purchased successfully:', item.value);
       res.status(200).json(updatedPlayer);
@@ -64,11 +99,13 @@ export default async (req: NextApiRequest, res: NextApiResponse)  => {
   if (!itemId || !playerEmail || !merchantId) {
     return res.status(400).json({ error: "Item Id , itemtype or playerEmail is required." });
   }
+
   try {
 
     await transferItemToPlayer(playerEmail, merchantId, itemId)
       .then((updatedPlayer) => {
-        console.log('Player updated:', updatedPlayer);
+        console.log('Player updated:', playerEmail , itemId);
+        res.status(200).json(updatedPlayer)
       })
       .catch((error) => {
         console.error('Purchase failed:', error.message);
