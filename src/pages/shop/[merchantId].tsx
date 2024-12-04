2e979aafd1536c7dcdf463827e9c9ea5ab54967d
ionpazoos <jon.pazos@ikasle.aeg.eus>
2024-12-04 10:43:37 +0100

    [EEE-326][UPDATE]updated object view on shop

--- src/pages/shop/[merchantId].tsx
+++ src/pages/shop/[merchantId].tsx
@@ -1,3 +1,5 @@
+// src/pages/shop/[merchantId].tsx
+
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
@@ -9,103 +11,101 @@ import MerchantInfo from '@/components/shop/MerchantInfo';
 import ItemStats from '@/components/shop/ItemStats';
 import ItemCard from '@/components/shop/ItemCard';
 import ItemDisplay from '@/components/shop/ItemDisplay';
+import ItemCarousel from '@/components/shop/ItemCarousel'; // Importa el nuevo componente
 import { calculateAllAttributes } from '@/helpers/PlayerAttributes';
 import { Modifier } from '@/_common/interfaces/Modifier';
 
 const MerchantPage: React.FC = () => {
-    const { data: session, status } = useSession();
-    const router = useRouter();
-    const { merchantId } = router.query;
-   
-    const [items, setItems] = useState<any>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [player, setPlayer] = useState<Player>();
-    const [error, setError] = useState<string | null>(null);
-    const [cartItems, setCartItems] = useState<any>([]);
-    const [availableMoney, setAvailableMoney] = useState<number | undefined>(0);
-    const [selectedItem, setSelectedItem] = useState<any | null>(null);
-    const [currentAttributes, setCurrentAttributes] = useState<Modifier>();
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const { merchantId } = router.query;
 
+  const [items, setItems] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [player, setPlayer] = useState<Player>();
+  const [error, setError] = useState<string | null>(null);
+  const [cartItems, setCartItems] = useState<any[]>([]);
+  const [availableMoney, setAvailableMoney] = useState<number>(0);
+  const [selectedItem, setSelectedItem] = useState<any | null>(null);
+  const [currentAttributes, setCurrentAttributes] = useState<Modifier>();
 
+  useEffect(() => {
+    if (status === 'loading' || !session || !merchantId) return;
 
-    useEffect(() => {
-      if (status === 'loading' || !session || !merchantId) return;
-      
-      const fetchShopItems = async () => {
-        try {
-          setLoading(true);
-          const response = await fetch(`/api/shop/merchants/${merchantId}`);
-          const itemsData = await response.json();
-          console.log(itemsData);
-          
-          setItems(itemsData);
-        } catch (error) {
-          console.error('Failed to fetch merchant items:', error);
-        } finally {
-            if (player) {
-              setLoading(false); // player is slower to fetch 
-            }
-        }
-      };
-    
-      fetchShopItems();
-    }, [merchantId]);
-  
-    useEffect(() => {
-      if (session?.user?.email) {
-        const fetchPlayerData = async () => {
-          try {
-            setLoading(true);
-            const res = await fetch(`/api/player/check-registration?email=${session.user?.email}`);
-            if (res.status === 200) {
-              const response = await res.json();
-              console.log(response.data)
-              setPlayer(response.data);
-              
-            } else if (res.status === 404) {
-              const response = await res.json();
-            } else {
-              setError('An error occurred while checking registration');
-            }
-          } catch (error) {
-            setError('An error occurred while checking registration');
-          } finally {
-            setLoading(false);
-          }
-        };
-  
-        fetchPlayerData();
-      }
-    }, [session]);
-    
-    useEffect(() => {
-      if(player) {
-      calculateAllAttributes(player, setCurrentAttributes);
-      setAvailableMoney(player?.gold)
-      }
-    }, [player])
-
-  const handleBuy = async(item:any, player:any) => {
-    console.log(item);
-    if (player.gold >= item.value) {
-      console.log("Available");
+    const fetchShopItems = async () => {
       try {
         setLoading(true);
+        const response = await fetch(`/api/shop/merchants/${merchantId}`);
+        const itemsData = await response.json();
+        console.log(itemsData);
 
-        if (player.gold >= item.value ) {
-          const res = await fetch(`/api/shop/${player.email}/${merchantId}/${item.name}`);
+        setItems(itemsData);
+      } catch (error) {
+        console.error('Failed to fetch merchant items:', error);
+      } finally {
+        if (player) {
+          setLoading(false); // player is slower to fetch
+        }
+      }
+    };
+
+    fetchShopItems();
+  }, [merchantId]);
+
+  useEffect(() => {
+    if (session?.user?.email) {
+      const fetchPlayerData = async () => {
+        try {
+          setLoading(true);
+          const res = await fetch(
+            `/api/player/check-registration?email=${session.user?.email}`
+          );
           if (res.status === 200) {
             const response = await res.json();
-            console.log(response)
-            setPlayer(response);
+            console.log(response.data);
+            setPlayer(response.data);
           } else if (res.status === 404) {
-            console.log(error);
+            const response = await res.json();
           } else {
             setError('An error occurred while checking registration');
           }
+        } catch (error) {
+          setError('An error occurred while checking registration');
+        } finally {
+          setLoading(false);
         }
+      };
 
-        
+      fetchPlayerData();
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (player) {
+      calculateAllAttributes(player, setCurrentAttributes);
+      setAvailableMoney(player?.gold || 0);
+    }
+  }, [player]);
+
+  const handleBuy = async (item: any, player: any) => {
+    console.log(item);
+    if (player.gold >= item.value) {
+      console.log('Available');
+      try {
+        setLoading(true);
+
+        const res = await fetch(
+          `/api/shop/${player.email}/${merchantId}/${item.name}`
+        );
+        if (res.status === 200) {
+          const response = await res.json();
+          console.log(response);
+          setPlayer(response);
+        } else if (res.status === 404) {
+          console.log(error);
+        } else {
+          setError('An error occurred while checking registration');
+        }
       } catch (error) {
         console.error('Failed to complete purchase:', error);
         setError('Failed to complete purchase');
@@ -113,54 +113,50 @@ const MerchantPage: React.FC = () => {
         setLoading(false);
       }
     } else {
-      console.log("Unavaliable");
-    }
-  }
-
-  const handleAddToCart = async(item:any, player:any) => {
-    
-    if (cartItems.some((cartItem:item) => cartItem._id === item._id)) {
-      console.log("already inside");
-      
-    } else {
-      if (availableMoney) {
-        if (availableMoney >= item.value) {
-          console.log("Available");
-          cartItems.push(item);
-          console.log(cartItems);
-          setAvailableMoney(availableMoney - item.value)
-        } else {
-          console.log("Unavaliable");
-        }
-      } else {
-        console.log("NO MONEY AVAILABLE"); 
-      }
-    }
-  }
-
-  const emptyCart = () => {
-    setCartItems([]);
-    setAvailableMoney(player?.gold)
-  }
-  interface item {
-      _id: number,
-      name: string,
-      image: string,
-      value: number,
-  }
-  const removeItem = (item: item) => {
-    if (availableMoney) {
-      setCartItems((cartItems: item[]) => cartItems.filter((cartItem: item) => cartItem._id !== item._id));
-      setAvailableMoney(availableMoney + item.value) 
-    } else {
-      console.log("available money undefined");
+      console.log('Unavailable');
     }
   };
 
-  const calculateTotalPrice = () => 
-      cartItems.reduce((total:any, item:item) => total + item.value, 0);
+  const handleAddToCart = (item: any, player: any) => {
+    if (cartItems.some((cartItem) => cartItem._id === item._id)) {
+      console.log('already inside');
+    } else {
+      if (availableMoney >= item.value) {
+        console.log('Available');
+        setCartItems([...cartItems, item]);
+        console.log(cartItems);
+        setAvailableMoney(availableMoney - item.value);
+      } else {
+        console.log('Unavailable');
+      }
+    }
+  };
+
+  const emptyCart = () => {
+    setCartItems([]);
+    setAvailableMoney(player?.gold || 0);
+  };
+
+  const removeItem = (item: any) => {
+    setCartItems((cartItems) =>
+      cartItems.filter((cartItem) => cartItem._id !== item._id)
+    );
+    setAvailableMoney((prev) => prev + item.value);
+  };
+
+  const calculateTotalPrice = () =>
+    cartItems.reduce((total, item) => total + item.value, 0);
 
   if (!session) return null;
+
+  const allItems: any[] = [];
+  if (items.length > 0) {
+    Object.entries(items[0]).forEach(([category, data]) => {
+      if (Array.isArray(data)) {
+        allItems.push(...data);
+      }
+    });
+  }
 
   return (
     <Layout>
@@ -178,60 +174,34 @@ const MerchantPage: React.FC = () => {
             merchantName={`Merchant ${merchantId}`}
           />
           <div className="">
-            <ItemStats selectedItem={selectedItem} atributtes={currentAttributes} playerLevel={player?.level}/>
+            <ItemStats
+              selectedItem={selectedItem}
+              atributtes={currentAttributes}
+              playerLevel={player?.level}
+            />
           </div>
         </div>
 
         {/* Contenido principal */}
         <div className="w-3/4 p-4">
           {error && <div className="text-red-600">{error}</div>}
-          <div className="fixed top-32 right-4 bg-white shadow-lg p-4 rounded-md border">
-            <h2 className="text-xl font-semibold">Balance</h2>
-            <div className="text-lg text-green-600">${player?.gold || 0}</div>
-          </div>
-          <h1 className="text-center text-2xl font-bold mb-4">
-            Items for Merchant {merchantId}
-          </h1>
-          {items.length > 0 ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {Object.entries(items[0]).map(([category, data]) => {
-                if (!Array.isArray(data)) return null;
 
-                return (
-                  <div key={category}>
-                    <h2 className="text-xl font-semibold mb-4">
-                      {category.charAt(0).toUpperCase() + category.slice(1)}
-                    </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                      {data.map((item: any, index: React.Key | null | undefined) => (
-                        <div
-                          key={index}
-                          onMouseEnter={() => setSelectedItem(item)} // Actualiza el objeto seleccionado al hacer hover
-                        >
-                          <ItemCard
-                            item={item}
-                            player={player}
-                            handleBuy={() => handleBuy(item, player)}
-                          />
-                          <button
-                            className="mt-auto bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                            onClick={() => handleAddToCart(item, player)}
-                          >
-                            Add to Cart
-                          </button>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          ) : (
-            <p>No items available for this merchant.</p>
-          )}
+          {/* Usamos el nuevo componente ItemCarousel */}
+          <ItemCarousel
+            items={allItems}
+            player={player}
+            handleBuy={handleBuy}
+            handleAddToCart={handleAddToCart}
+            setSelectedItem={setSelectedItem}
+          />
         </div>
       </div>
-        <ItemDisplay items={cartItems} emptyCart={emptyCart} removeItem={removeItem} calculateTotalPrice={calculateTotalPrice}/>
+      <ItemDisplay
+        items={cartItems}
+        emptyCart={emptyCart}
+        removeItem={removeItem}
+        calculateTotalPrice={calculateTotalPrice}
+      />
     </Layout>
   );
 };
