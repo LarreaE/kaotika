3abf9849916ecaf39c693fec78b30df2ac89fae3
ionpazoos <jon.pazos@ikasle.aeg.eus>
2024-12-04 08:30:34 +0100

    [EEE-315][UPDATE]update merchat image

--- src/components/MerchantInfo.tsx
+++ src/components/MerchantInfo.tsx
@@ -8,12 +8,26 @@ interface MerchantInfoProps {
 const MerchantInfo: React.FC<MerchantInfoProps> = ({ merchantImage, merchantName }) => {
   return (
     <div className="flex flex-col items-center p-4 text-black">
-      <img
-        src={merchantImage || "/placeholder-merchant.jpg"}
-        alt={merchantName || "Merchant"}
-        className="w-32 h-32 object-cover rounded-full mb-4"
-      />
-      <h2 className="text-lg font-bold">{merchantName || "Unnamed Merchant"}</h2>
+      {/* Contenedor relativo para permitir posicionamiento absoluto */}
+      <div className="relative w-[55%] h-[55%]">
+        {/* Imagen principal (vendedor) */}
+        <img
+          src={merchantImage || "/placeholder-merchant.jpg"}
+          alt={merchantName || "Merchant"}
+          className="w-full h-full object-cover rounded-full"
+        />
+        </div>
+        {/* Imagen superpuesta */}
+        <div className="absolute w-[40%] h-[40%] bottom-[45%]">
+        <img
+          src="/images/seller_border.png" // Cambia la ruta a la imagen que quieres superponer
+          alt="Overlay"
+          className="absolute top-0 left-0 w-full h-full object-contain pointer-events-none"
+        />
+        </div>
+
+      {/* Nombre del mercader */}
+      <h2 className="text-2xl font-bold mt-14">{merchantName || "Unnamed Merchant"}</h2>
     </div>
   );
 };
