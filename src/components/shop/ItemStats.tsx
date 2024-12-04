a6f1226eb669d2650702245305d1ec418dd84d9f
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-12-04 12:20:42 +0100

    [MERGE] fixed merge issues

---
+++ src/components/shop/ItemStats.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import ProgressBar from '../ProgressBar';
+
+import { Weapon } from '../../_common/interfaces/Weapon';
+import { Armor } from '../../_common/interfaces/Armor';
+import { Boot } from '@/_common/interfaces/Boot';
+import { Helmet } from '@/_common/interfaces/Helmet';
+import { Artifact } from '@/_common/interfaces/Artifact';
+import { Ring } from '@/_common/interfaces/Ring';
+import { Shield } from '@/_common/interfaces/Shield';
+import isModifier from '@/helpers/IsModifier';
+import { Attribute } from '@/_common/interfaces/Attribute';
+import { Modifier } from '@/_common/interfaces/Modifier';
+
+type Item = Weapon | Armor | Boot | Helmet | Artifact | Ring | Shield; 
+
+interface ItemStatsProps {
+  selectedItem: Item | null; 
+  atributtes: Modifier | undefined;
+  playerLevel: number | undefined;
+}
+
+const ItemStats: React.FC<ItemStatsProps> = ({ selectedItem, atributtes, playerLevel }) => {
+  if (!selectedItem) {
+    return (
+      <div className="text-center text-gray-500 italic">
+        Selecciona un elemento para ver sus estadísticas.
+      </div>
+    );
+  }
+
+  const renderAttributeProgressBars = () => {
+    return Object.entries(atributtes).map(([attrName, baseValue]) => {
+      // Obtener el modificador del objeto seleccionado para este atributo
+      let modifierValue = 0;
+
+      Object.entries(selectedItem).forEach(([key, value]) => {
+        if (typeof value === 'object' && isModifier(value)) {
+          Object.entries(value).forEach(([modKey, modValue]) => {
+            if (modKey === attrName) {
+              modifierValue += modValue;
+            }
+          });
+        }
+      });
+
+      const totalValue = baseValue + modifierValue;
+
+      // Determinar el valor máximo para la barra de progreso
+      const maxValue = Math.max(baseValue, totalValue, 100);
+
+      // Crear el label que muestra el atributo, valor base y modificador
+      const label = `${attrName.toUpperCase()}: ${baseValue} ${modifierValue !== 0 ? (modifierValue > 0 ? `+${modifierValue}` : `${modifierValue}`) : ''}`;
+
+      return (
+        <div key={attrName} className="mb-4">
+          <ProgressBar
+            label={label}
+            value={totalValue}
+            maxValue={maxValue}
+          />
+        </div>
+      );
+    });
+  };
+
+  // Obtener el nivel mínimo requerido del objeto
+  const itemMinLevel = selectedItem.min_lvl || 1; // Asegúrate de que la propiedad sea correcta
+
+  // Determinar el color según la comparación de niveles
+  const levelColor = itemMinLevel <= playerLevel ? 'text-green-400' : 'text-red-400';
+
+  return (
+    <div className="p-4 bg-gray-900 rounded shadow-md text-white border-2 border-yellow-500">
+      <div className="flex justify-between items-center">
+        <h3 className="text-3xl font-semibold">{selectedItem.name}</h3>
+        <p className={`text-xl font-semibold ${levelColor}`}>
+          Min lvl: {itemMinLevel}
+        </p>
+      </div>
+      {renderAttributeProgressBars()}
+    </div>
+  );
+};
+
+export default ItemStats;
--- src/pages/shop/[merchantId].tsx
+++ src/pages/shop/[merchantId].tsx
@@ -1,3 +1,5 @@
+// src/pages/shop/[merchantId].tsx
+
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
@@ -5,105 +7,113 @@ import Layout from '../../components/Layout';
 import Loading from '../../components/Loading';
 import { Player } from '@/_common/interfaces/Player';
 
-import MerchantInfo from '@/components/MerchantInfo';
-import ItemStats from '@/components/ItemStats';
-import ItemCard from '@/components/ItemCard';
+import MerchantInfo from '@/components/shop/MerchantInfo';
+import ItemStats from '@/components/shop/ItemStats';
+import ItemCard from '@/components/shop/ItemCard';
 import ItemDisplay from '@/components/shop/ItemDisplay';
+import ItemCarousel from '@/components/shop/ItemCarousel'; // Importa el nuevo componente
+import { calculateAllAttributes } from '@/helpers/PlayerAttributes';
+import { Modifier } from '@/_common/interfaces/Modifier';
 
 const MerchantPage: React.FC = () => {
 
-    const { data: session, status } = useSession();
-    const router = useRouter();
-    const { merchantId } = router.query;
-   
-    const [items, setItems] = useState<any>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [player, setPlayer] = useState<Player>();
-    const [error, setError] = useState<string | null>(null);
-    const [cartItems, setCartItems] = useState<any>([]);
-    const [availableMoney, setAvailableMoney] = useState<number | undefined>(0);
-    const [selectedItem, setSelectedItem] = useState<any | null>(null); // Estado para el objeto seleccionado
+  interface item {
+    _id: number,
+    name: string,
+    image: string,
+    value: number,
+}
 
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const { merchantId } = router.query;
 
-    useEffect(() => {
-      if (status === 'loading' || !session || !merchantId) return;
-      
-      const fetchShopItems = async () => {
-        try {
-          setLoading(true);
-          const response = await fetch(`/api/shop/merchants/${merchantId}`);
-          const itemsData = await response.json();
-          console.log(itemsData);
-          
-          setItems(itemsData);
-        } catch (error) {
-          console.error('Failed to fetch merchant items:', error);
-        } finally {
-            if (player) {
-              setLoading(false); // player is slower to fetch 
-            }
-        }
-      };
-    
-      fetchShopItems();
-    }, [merchantId]);
-  
-    useEffect(() => {
-      if (session?.user?.email) {
-        const fetchPlayerData = async () => {
-          try {
-            setLoading(true);
-            const res = await fetch(`/api/player/check-registration?email=${session.user?.email}`);
-            if (res.status === 200) {
-              const response = await res.json();
-              console.log(response)
-              setPlayer(response);
-              
-            } else if (res.status === 404) {
-              const response = await res.json();
-            } else {
-              setError('An error occurred while checking registration');
-            }
-          } catch (error) {
-            setError('An error occurred while checking registration');
-          } finally {
-            setLoading(false);
-          }
-        };
-  
-        fetchPlayerData();
-      }
-    }, [session]);
+  const [items, setItems] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [player, setPlayer] = useState<Player>();
+  const [error, setError] = useState<string | null>(null);
+  const [cartItems, setCartItems] = useState<any>([]);
+  const [availableMoney, setAvailableMoney] = useState<number>(0);
+  const [selectedItem, setSelectedItem] = useState<any | null>(null);
+  const [currentAttributes, setCurrentAttributes] = useState<Modifier>();
 
-    useEffect(() => {
-      setAvailableMoney(player?.gold)
-      console.log(availableMoney);
-    }, [player])
-    useEffect(() => {
-      console.log(availableMoney);
-    }, [availableMoney])
-    
-  const handleBuy = async(item:any, player:any) => {
-    console.log(item);
-    if (player.gold >= item.value) {
-      console.log("Available");
+  useEffect(() => {
+    if (status === 'loading' || !session || !merchantId) return;
+
+    const fetchShopItems = async () => {
       try {
         setLoading(true);
+        const response = await fetch(`/api/shop/merchants/${merchantId}`);
+        const itemsData = await response.json();
+        console.log(itemsData);
 
-        if (player.gold >= item.value ) {
-          const res = await fetch(`/api/shop/${player.email}/${merchantId}/${item.name}`);
+        setItems(itemsData);
+      } catch (error) {
+        console.error('Failed to fetch merchant items:', error);
+      } finally {
+        if (player) {
+          setLoading(false); // player is slower to fetch
+        }
+      }
+    };
+
+    fetchShopItems();
+  }, [merchantId]);
+
+  useEffect(() => {
+    if (session?.user?.email) {
+      const fetchPlayerData = async () => {
+        try {
+          setLoading(true);
+          const res = await fetch(
+            `/api/player/check-registration?email=${session.user?.email}`
+          );
           if (res.status === 200) {
             const response = await res.json();
-            console.log(response)
+            console.log(response);
             setPlayer(response);
           } else if (res.status === 404) {
-            console.log(error);
+            const response = await res.json();
           } else {
             setError('An error occurred while checking registration');
           }
+        } catch (error) {
+          setError('An error occurred while checking registration');
+        } finally {
+          setLoading(false);
         }
+      };
 
-        
+      fetchPlayerData();
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (player) {
+      calculateAllAttributes(player, setCurrentAttributes);
+      setAvailableMoney(player?.gold || 0);
+    }
+  }, [player]);
+
+  const handleBuy = async (item: any, player: any) => {
+    console.log(item);
+    if (player.gold >= item.value) {
+      console.log('Available');
+      try {
+        setLoading(true);
+
+        const res = await fetch(
+          `/api/shop/${player.email}/${merchantId}/${item.name}`
+        );
+        if (res.status === 200) {
+          const response = await res.json();
+          console.log(response);
+          setPlayer(response);
+        } else if (res.status === 404) {
+          console.log(error);
+        } else {
+          setError('An error occurred while checking registration');
+        }
       } catch (error) {
         console.error('Failed to complete purchase:', error);
         setError('Failed to complete purchase');
@@ -111,41 +121,30 @@ const MerchantPage: React.FC = () => {
         setLoading(false);
       }
     } else {
-      console.log("Unavaliable");
+      console.log('Unavailable');
     }
-  }
+  };
 
-  const handleAddToCart = async(item:any, player:any) => {
-    
+  const handleAddToCart = (item: any, player: any) => {
     if (cartItems.some((cartItem:item) => cartItem._id === item._id)) {
-      console.log("already inside");
-      
+      console.log('already inside');
     } else {
-      if (availableMoney) {
-        if (availableMoney >= item.value) {
-          console.log("Available");
-          cartItems.push(item);
-          console.log(cartItems);
-          setAvailableMoney(availableMoney - item.value)
-        } else {
-          console.log("Unavaliable");
-        }
+      if (availableMoney >= item.value) {
+        console.log('Available');
+        setCartItems([...cartItems, item]);
+        console.log(cartItems);
+        setAvailableMoney(availableMoney - item.value);
       } else {
-        console.log("NO MONEY AVAILABLE"); 
+        console.log('Unavailable');
       }
     }
-  }
+  };
 
   const emptyCart = () => {
     setCartItems([]);
-    setAvailableMoney(player?.gold)
-  }
-  interface item {
-      _id: number,
-      name: string,
-      image: string,
-      value: number,
-  }
+    setAvailableMoney(player?.gold || 0);
+  };
+
   const removeItem = (item: item) => {
     if (availableMoney) {
       setCartItems((cartItems: item[]) => cartItems.filter((cartItem: item) => cartItem._id !== item._id));
@@ -160,10 +159,19 @@ const MerchantPage: React.FC = () => {
     router.push(`/shop/checkout?cart=${encodedCartItems}`);
   };
 
-  const calculateTotalPrice = () => 
-      cartItems.reduce((total:any, item:item) => total + item.value, 0);
+  const calculateTotalPrice = () =>
+    cartItems.reduce((total:number, item:item) => total + item.value, 0);
 
   if (!session) return null;
+
+  const allItems: any[] = [];
+  if (items.length > 0) {
+    Object.entries(items[0]).forEach(([category, data]) => {
+      if (Array.isArray(data)) {
+        allItems.push(...data);
+      }
+    });
+  }
 
   return (
     <Layout>
@@ -181,60 +189,35 @@ const MerchantPage: React.FC = () => {
             merchantName={`Merchant ${merchantId}`}
           />
           <div className="">
-            <ItemStats selectedItem={selectedItem} />
+            <ItemStats
+              selectedItem={selectedItem}
+              atributtes={currentAttributes}
+              playerLevel={player?.level}
+            />
           </div>
         </div>
 
         {/* Contenido principal */}
         <div className="w-3/4 p-4">
           {error && <div className="text-red-600">{error}</div>}
-          <div className="fixed top-32 right-4 bg-white shadow-lg p-4 rounded-md border">
-            <h2 className="text-xl font-semibold">Balance</h2>
-            <div className="text-lg text-green-600">${player?.gold || 0}</div>
-          </div>
-          <h1 className="text-center text-2xl font-bold mb-4">
-            Items for Merchant {merchantId}
-          </h1>
-          {items.length > 0 ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {Object.entries(items[0]).map(([category, data]) => {
-                if (!Array.isArray(data)) return null;
 
-                return (
-                  <div key={category}>
-                    <h2 className="text-xl font-semibold mb-4">
-                      {category.charAt(0).toUpperCase() + category.slice(1)}
-                    </h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                      {data.map((item: any, index: React.Key | null | undefined) => (
-                        <div
-                          key={index}
-                          onMouseEnter={() => setSelectedItem(item)} // Actualiza el objeto seleccionado al hacer hover
-                        >
-                          <ItemCard
-                            item={item}
-                            player={player}
-                            handleBuy={() => handleBuy(item, player)}
-                          />
-                          <button
-                            className="mt-auto bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                            onClick={() => handleAddToCart(item, player)}
-                          >
-                            Add to Cart
-                          </button>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          ) : (
-            <p>No items available for this merchant.</p>
-          )}
+          {/* Usamos el nuevo componente ItemCarousel */}
+          <ItemCarousel
+            items={allItems}
+            player={player}
+            handleBuy={handleBuy}
+            handleAddToCart={handleAddToCart}
+            setSelectedItem={setSelectedItem}
+          />
         </div>
       </div>
-        <ItemDisplay items={cartItems} emptyCart={emptyCart} removeItem={removeItem} calculateTotalPrice={calculateTotalPrice} goToCheckout={goToCheckout}/>
+      <ItemDisplay
+        items={cartItems}
+        emptyCart={emptyCart}
+        removeItem={removeItem}
+        calculateTotalPrice={calculateTotalPrice}
+        goToCheckout={goToCheckout}
+      />
     </Layout>
   );
 };
