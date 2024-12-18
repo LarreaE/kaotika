aae8160ca3f1ea86040a6e72b06f9afa7d44f595
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2024-12-18 09:51:38 +0100

    [EEE-355][FIX] fix the start of the website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from 'next/app';
 import "../styles/globals.css";
-import {NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
 import Head from "next/head";
-
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   const mockSession = {
     user: {
       name: "ENEKO LARREA PEREZ",
@@ -18,17 +21,23 @@ function MyApp({ Component, pageProps }: AppProps) {
     email: "eneko.larrea@ikasle.aeg.eus",
   };
 
+  useEffect(() => {
+    // Redirigir al usuario a /player si está en la raíz
+    if (router.pathname === '/') {
+      router.push('/player');
+    }
+  }, [router]);
+
   return (
     <SessionProvider session={mockSession}>
       <NextUIProvider>
-          <Head>
-            <link rel='icon' href='/favicon.ico' />
-          </Head>
-          <Component {...pageProps} />
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
       </NextUIProvider>
     </SessionProvider>
   );
-
 }
 
 export default MyApp;
